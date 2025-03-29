fd6192820c8d9e747cbb4596f4fb2b939903d6b7
ASHUTOSH <gangajal166@gmail.com>
2025-03-30 00:08:16 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
 import UserListPage from './pages/UserListPage';
 import LoginForm from './components/LogInForm';
-
 
 const App = () => (
   <Router>
